f590decff0d431e596334b29d2fba0fd3adcd0b0
Eli Orille <eli.orille@yahoo.com>
2022-07-29 10:40:53 -0500

    Made button asking for size

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-for(let i = 0; i<16; i++){
-    const container = document.createElement('div');
-    container.classList.add('container');
-    document.body.appendChild(container);
-}
-
-const rows = document.querySelectorAll('.container');
-const boxes = document.querySelectorAll('.content');
-rows.forEach(element => {
-    for(let i = 0; i<16; i++){
-        const content = document.createElement('div');
-        content.classList.add('content');
-        element.appendChild(content)
+function createGrid(size){
+    const a = document.querySelector('.okok');
+    for(let i = 0; i<size; i++){
+        const container = document.createElement('div');
+        container.classList.add('container');
+        a.appendChild(container);
     }
-});
 
-function write(){
-    boxes.classList.add('write')
+    const rows = document.querySelectorAll('.container');
+    rows.forEach(element => {
+        for(let i = 0; i<size; i++){
+            const content = document.createElement('div');
+            content.classList.add('content');
+            element.appendChild(content)
+        }
+    });
 }
 
+function colorBoxes(){
+let boxes = document.querySelectorAll('.content');
 boxes.forEach(box => {
-    box.addEventListener('click', () =>{
-        box.classList.remove('.content');
-        box.classList.add('.write');
+    box.addEventListener('mouseover',()=>{
+        box.setAttribute('style','background-color: black');
     });
 });
+}
 
+function askSize(){
+    let button = document.querySelector('button');
+    button.addEventListener('click',()=>{
+        let input = prompt("What size would you like");
+        createGrid(input);
+        colorBoxes();
+    });
+}
 
+askSize();
